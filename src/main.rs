81d0f7bc7bb0941181be8c3cdac6b1ac372e1b3f
Your Name <shlokjagtap123@gmail.com>
2023-09-12 10:13:01 +0530

    api fixing

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@ extern crate slack;
 extern crate chrono;
 extern crate dotenv;
 
-
-// use chrono::Utc;
 use slack::{Event, EventHandler, Message, RtmClient};
 use dotenv::dotenv;
 
-
 fn main() {
-        dotenv().ok();
+    dotenv().ok();
 
- let api_key = dotenv::var("SLACK_BOT_TOKEN")
+    let api_key = dotenv::var("SLACK_BOT_TOKEN")
         .expect("SLACK_BOT_TOKEN not found in environment variables");
-    let mut handler = Handler;
+    
+    // Read the SLACK_CHANNEL environment variable
+    let channel = dotenv::var("SLACK_CHANNEL")
+        .expect("SLACK_CHANNEL not found in environment variables");
+
+    let mut handler = Handler { channel };
     let r = RtmClient::login_and_run(&api_key, &mut handler);
 
     match r {
@@ -22,13 +24,9 @@ fn main() {
     }
 }
 
-// fn api_key() -> String {
-//     // Replace this with your logic to fetch the Slack API key.
-//     // For this example, we'll return a dummy API key.
-//     "xapp-1-A05RN086VNF-5872111710418-e2ee168785e9f74c9262df94b0c441981ae3ae62401687bdb9b3ee7b9597c738".to_string()
-// }
-
-pub struct Handler;
+pub struct Handler {
+    channel: String, // Store the channel ID or name
+}
 
 impl EventHandler for Handler {
     fn on_event(&mut self, cli: &RtmClient, event: Event) {
@@ -52,7 +50,6 @@ impl EventHandler for Handler {
 impl Handler {
     fn handle_message(&mut self, message: Box<Message>, cli: &RtmClient) {
         if let Message::Standard(message_standard) = *message {
-            let channel = message_standard.channel.unwrap();
             let user_id = message_standard.user.unwrap();
             let text = message_standard.text.unwrap();
 
@@ -65,7 +62,9 @@ impl Handler {
                 let current_year = 2023;
                 let age = current_year - birth_year;
                 let response = format!("Your age is {} years.", age);
-                send_message(&user_id, &response, &channel, &cli);
+                
+                // Use the specified channel when sending the message
+                send_message(&user_id, &response, &self.channel, &cli);
             } else {
                 println!("Invalid year of birth format");
             }
